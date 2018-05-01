7258afc8d6bf9e113e784c69f3af7aa5a7ebe7f0
Dave Longley <dlongley@digitalbazaar.com>
2018-05-01 13:43:36 -0400

    Remove unused html.

--- BrApp.vue
+++ BrApp.vue
@@ -3,12 +3,6 @@
     <br-header></br-header>
     <router-view></router-view>
     <br-footer></br-footer>
-
-    <!-- <br-route-loading v-if="route.changing"></br-route-loading>
-    <div v-else>
-      <router-view></router-view>
-      <br-footer></br-footer>
-    </div> -->
   </br-root>
 </template>
 <script>
