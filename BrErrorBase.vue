501fdbbcddbeaa5a93488c70f366587df94945b6
David I. Lehn <dlehn@digitalbazaar.com>
2019-10-25 14:41:34 -0400

    Handle HTTP meta error codes.
    - Add errors to config.
    - Add error pages.
    - Add custom base error page component name to config.
    - Use custom endpoints to display error when found on initial load.
    - Use common `bedrock-vue-core` for most dynamicly loaded chunks.

---
+++ BrErrorBase.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2><slot name="title" /></h2>
+    <slot />
+    <slot name="contact" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'BrErrorBase',
+};
+</script>
+<style>
+</style>
---
+++ HttpError503.vue
@@ -0,0 +1,20 @@
+<template>
+  <http-error>
+    <template v-slot:title>
+      Service Unavailable
+    </template>
+
+    <p>Sorry, this service is temporarily unavailable.</p>
+  </http-error>
+</template>
+<script>
+import HttpError from './HttpError.vue';
+export default {
+  name: 'HttpError503',
+  components: {
+    HttpError
+  }
+};
+</script>
+<style>
+</style>
